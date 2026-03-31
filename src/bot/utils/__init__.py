8fc5f4eaf37188fc7dc197430d65b0ee55931bff
konstantin <konstantin.sorokin.work@gmail.com>
2026-03-31 15:02:20 +0300

    Add: commands for menu bottom left

---
+++ src/bot/utils/__init__.py
@@ -0,0 +1,7 @@
+from bot.utils.bot_commands import COMMANDS
+from bot.utils.config import settings
+
+__all__ = [
+    "settings",
+    'COMMANDS'
+]
---
+++ src/bot/utils/bot_commands.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+COMMANDS = [
+    BotCommand(command="start", description="🏠 Главное меню"),
+    BotCommand(command="help", description="ℹ️ Помощь"),
+]
