bdeb6cd89aac0cd87afd38c9dad3c629083c2acd
konstantin <konstantin.sorokin.work@gmail.com>
2026-06-09 15:45:10 +0300

    Refactor: structure CardService and add documentation

--- src/bot/services/card_service.py
+++ src/bot/services/card_service.py
@@ -5,33 +5,37 @@ from bot.utils import RedisKeys
 
 
 class CardService(BaseService):
-    async def get_next_translation_card(
-        self, telegram_id: int, user_id: int
-    ) -> dict | None:
+    async def initialize_learning_queue(self, telegram_id: int, user_id: int) -> bool:
+        """
+        Загружает карточки из БД в Redis.
+        Возвращает True, если карточки найдены.
+        """
         queue_key = RedisKeys.user_cards(telegram_id)
 
+        await self.redis.delete(queue_key)
+
+        cards = await self._request(method="GET", endpoint=f"users/{user_id}/progress/")
+
+        if not cards:
+            return False
+
+        await self.add_cards_to_queue(telegram_id, cards)
+        return True
+
+    async def get_next_card_from_queue(self, telegram_id: int) -> dict | None:
+        """Достает следующую(первую слева) карту из Redis."""
+        queue_key = RedisKeys.user_cards(telegram_id)
         cache_card = await self.redis.lpop(queue_key)
-        if cache_card:
-            return json.loads(cache_card)
-
-        db_cards = await self._request(
-            method="GET",
-            endpoint=f"users/{user_id}/progress/",
-        )
-
-        if not db_cards:
-            return None
-
-        await self.add_cards_to_queue(telegram_id, cards=db_cards)
-
-        cache_card = await self.redis.lpop(queue_key)
-        return json.loads(cache_card)
+        return json.loads(cache_card) if cache_card else None
 
     async def add_card_to_the_end_queue(self, telegram_id: int, card: dict):
+        """Кладет карту в конец очереди Redis.
+        Функция нужна в момент, когда пользователь не знает карточку"""
         queue_key = RedisKeys.user_cards(telegram_id)
         return await self.redis.rpush(queue_key, json.dumps(card, ensure_ascii=False))
 
     async def add_cards_to_queue(self, telegram_id: int, cards: list[dict]):
+        """Кладет карту в начало очереди Redis."""
         queue_key = RedisKeys.user_cards(telegram_id)
         pipe = self.redis.pipeline()
         for card in cards:
@@ -39,6 +43,7 @@ class CardService(BaseService):
         await pipe.execute()
 
     async def load_pack_verbs_to_cache(self, telegram_id: int, pack_id: int):
+        """Берет все паки для неправильных глаголов из БД и добавляет в Redis"""
         cards: list[dict] = await self._request(
             method="GET",
             endpoint=f"packs/{pack_id}/cards/",
