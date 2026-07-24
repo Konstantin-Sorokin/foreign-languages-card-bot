74d73e25cf0ce83db26f53093eb567b6b805df7b
konstantin <konstantin.sorokin.work@gmail.com>
2026-07-24 16:16:22 +0300

    refactor: update router to include add and learning handlers, remove unused callbacks and messages routers

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,12 +1,15 @@
 from aiogram import Router
 
-from bot.handlers.callbacks import router as callbacks_router
+from bot.handlers.add import router as add_router
 from bot.handlers.commands import router as commands_router
-from bot.handlers.messages import router as messages_router
+from bot.handlers.learning import router as learning_router
 
 router = Router()
 
-router.include_routers(commands_router, messages_router, callbacks_router)
+router.include_router(commands_router)
+router.include_router(learning_router)
+router.include_router(add_router)
+
 
 __all__ = [
     "router",
